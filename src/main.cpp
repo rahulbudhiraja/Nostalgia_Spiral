55d4abc7d5d2798bc21aa35bcef201cfdebe81e7
Rahul Budhiraja <rahul.budhiraja.dark@gmail.com>
2013-03-08 21:34:11 -0500

    Spiral 8/03/2013

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
-#include "ofMain.h"
-#include "cvEyeTracker.h"
+#include "testApp.h"
 #include "ofAppGlutWindow.h"
 
-//========================================================================
-int main( ){
-    
-    ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1680, 1000, OF_WINDOW);
-	ofRunApp( new cvEyeTracker() );
-
+//--------------------------------------------------------------
+int main(){
+	ofAppGlutWindow window; // create a window
+	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
+	ofSetupOpenGL(&window, 1024, 768, OF_WINDOW);
+	ofRunApp(new testApp()); // start the app
 }
